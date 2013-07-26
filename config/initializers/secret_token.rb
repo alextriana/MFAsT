6f7c73cb00c3e9bbefb8abfd83e6e79428b615da
Alex Triana <alex.triana2@gmail.com>
2013-07-25 21:44:03 -0700

    first commit-nav

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mfast::Application.config.secret_token = '3c1542f887234cf355bbf22533d0ffc05344fdbe0e4322c0e2070ad3c4d882ecfff7076c4e213f13c5f6eb19be7fc8cfd3c265ec3647b931a0d3686f02271c04'
