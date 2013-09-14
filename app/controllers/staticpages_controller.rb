2426bbdf04894acdab1e88f9e77a569fe792c85d
Alex Triana <alex.triana2@gmail.com>
2013-09-13 19:11:24 -0700

    done

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -5,21 +5,5 @@ class StaticpagesController < ApplicationController
     @subscriber=Subscriber.new
   end
 
-  def test
-  @feedback=Feedback.new
-    @betaUser=BetaUser.new
-    @subscriber=Subscriber.new
-
-    end
-
-    def test2
-  @feedback=Feedback.new
-    @betaUser=BetaUser.new
-    @subscriber=Subscriber.new
-
-    end
-
-    def greek
-      
-    end
+ 
 end
